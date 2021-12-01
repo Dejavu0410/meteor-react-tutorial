52449aacfd62e3964106293f75643ced17cfd5a0
Dejavu0410 <m.farukkilinc05@gmail.com>
2021-12-01 23:31:35 +0300

    Example meteor react stuff

---
+++ client/Components/Header.jsx
@@ -0,0 +1,13 @@
+Header = React.createClass({
+   render(){
+       return (
+           <div>
+               <div className="navbar navbar-Default">
+                   <div className="container">
+                       <a className="navbar-brand">My site name</a>
+                   </div>
+               </div>
+           </div>
+       )
+   }
+});
